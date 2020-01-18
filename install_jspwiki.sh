7dca5681b7ca591a25c8388272bba6e15bd0091a
Thai Dang <>
2020-01-19 00:47:26 +0100

    update

--- install_jspwiki.sh
+++ install_jspwiki.sh
@@ -4,4 +4,4 @@ mkdir -p $HOME/run/jspwiki/pages
 chmod 755 $HOME/run/jspwiki/pages
 chown -R 1000:1000 $HOME/run/jspwiki/pages
 
-podman run -d -p 2004:8080 --name myjspwiki --env="jspwiki_baseURL=http://localhost/" -v=$HOME/run/jspwiki/pages:/var/jspwiki/pages:z docker.io/metskem/docker-jspwiki:2.11.0-M5
+podman run -d -p 2004:8080 --name myjspwiki --network=mynetwork --env="jspwiki_baseURL=http://localhost/" -v=$HOME/run/jspwiki/pages:/var/jspwiki/pages:z docker.io/metskem/docker-jspwiki:2.11.0-M5
