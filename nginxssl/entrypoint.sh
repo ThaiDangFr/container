d6a090e6e881c81af508de95b06202f704d4dd91
Thai Dang <root@mercure.dangconsulting.fr>
2020-01-21 16:31:23 +0100

    correct ENVT=TEST

--- nginxssl/entrypoint.sh
+++ nginxssl/entrypoint.sh
@@ -5,18 +5,21 @@ SUBDOMAINS="guacamole acacias etherpad jspwiki"
 certbot plugins --init
 certbot plugins --prepare
 
+OPTIONS="--test-cert"
 if [ $ENVT == "PROD" ];then
-    nginx
-    sleep 1
-
-    for s in $SUBDOMAINS; do
-	echo "certbot --nginx -d $s.dangconsulting.fr --email "ssl@dangconsulting.fr" --agree-tos --no-eff-email"
-	certbot --nginx -d $s.dangconsulting.fr --email "ssl@dangconsulting.fr" --agree-tos --no-eff-email
-	sleep 1
-    done
-
-    nginx -s stop
-    sleep 1
+    OPTIONS=""
 fi
+
+nginx
+sleep 1
+
+for s in $SUBDOMAINS; do
+    echo "certbot --nginx -d $s.dangconsulting.fr --email "ssl@dangconsulting.fr" --agree-tos --no-eff-email $OPTIONS"
+    certbot --nginx -d $s.dangconsulting.fr --email "ssl@dangconsulting.fr" --agree-tos --no-eff-email $OPTIONS
+    sleep 1
+done
+
+nginx -s stop
+sleep 1
 
 nginx -t && nginx -g "daemon off;"
