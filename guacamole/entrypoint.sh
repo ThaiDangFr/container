cb77f9f2344301828b1145bf1b7a7fdd8181713c
Thai Dang <root@mercure.dangconsulting.fr>
2020-01-21 17:25:22 +0100

    update

--- guacamole/entrypoint.sh
+++ guacamole/entrypoint.sh
@@ -2,49 +2,54 @@
 
 source $HOME/.bashrc
 
-cat <<EOF > $HOME/.Xclients 
+echo "mycentosvnc" | passwd --stdin root
+
+cat <<EOF > /home/guac/.Xclients 
 xsetroot -solid grey
 xterm -geometry 80x24+10+10 -ls -title "\$VNCDESKTOP Desktop" &
 startfluxbox &
 EOF
 
-chmod 755 $HOME/.Xclients
+chmod 755 /home/guac/.Xclients
+chown guac:guac /home/guac/.Xclients
 
-mkdir $HOME/.vnc
-chmod 755 $HOME/.vnc
-echo 'mycentosvnc' | vncpasswd -f > $HOME/.vnc/passwd
-chmod 600 $HOME/.vnc/passwd
+su - guac -c "mkdir /home/guac/.vnc"
+chmod 755 /home/guac/.vnc
+su - guac -c "echo 'mycentosvnc' | vncpasswd -f > /home/guac/.vnc/passwd"
+chmod 600 /home/guac/.vnc/passwd
 
-cat <<EOF > $HOME/.vnc/xstartup
+cat <<EOF > /home/guac/.vnc/xstartup
 #!/bin/sh
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
 /etc/X11/xinit/xinitrc
 EOF
 
-chmod 755 $HOME/.vnc/xstartup
+chmod 755 /home/guac/.vnc/xstartup
+chown guac:guac /home/guac/.vnc/xstartup
 
-mkdir $HOME/bin
+su - guac -c "mkdir /home/guac/bin"
 
-cat <<EOF > $HOME/bin/switchHome.sh
+cat <<EOF > /home/guac/bin/switchHome.sh
 #!/bin/bash
 xrandr --output VNC-0 --mode 1920x1080
 EOF
 
-cat <<EOF > $HOME/bin/switchWork.sh
+cat <<EOF > /home/guac/bin/switchWork.sh
 #!/bin/bash
 xrandr --output VNC-0 --mode 1280x1024
 EOF
 
-chmod 755 $HOME/bin/switchHome.sh $HOME/bin/switchWork.sh
+chmod 755 /home/guac/bin/switchHome.sh /home/guac/bin/switchWork.sh
+chown guac:guac /home/guac/bin/switchHome.sh /home/guac/bin/switchWork.sh
 
 echo "remove old vnc locks to be a reattachable container"
-vncserver -kill :1 || rm -rfv /tmp/.X*-lock /tmp/.X11-unix || echo "no locks present"
+su - guac -c "vncserver -kill :1" || rm -rfv /tmp/.X*-lock /tmp/.X11-unix || echo "no locks present"
 
-#vncserver :1 -geometry 1024x768 -nolisten tcp -localhost
-vncserver :1 -geometry 1024x768
+#su - guac -c "vncserver :1 -geometry 1024x768 -nolisten tcp -localhost"
+su - guac -c "vncserver :1 -geometry 1024x768"
 sleep 5
-tail -f $HOME/.vnc/*:1.log
+tail -f /home/guac/.vnc/*:1.log
 
 
 
