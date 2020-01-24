7acc7ac615c75b87f7e94c674a0d05e3dbac4b29
Thai Dang <root@mercure.dangconsulting.fr>
2020-01-24 16:54:47 +0100

    totp

--- install_guacamole.sh
+++ install_guacamole.sh
@@ -11,13 +11,14 @@ chown 999:999 $HOME/run/guacamole/pgdata
 echo "1) Creating guacd container"
 #podman pull docker.io/guacamole/guacd:1.0.0
 #podman run --name myguacd --hostname myguacd --ip $GUACD_IP -d guacamole/guacd:1.0.0
+# la 0.9.14 est plus stable que la 1.0.0 (souvent des messages de deconnection)
 podman pull docker.io/guacamole/guacd:0.9.14
 podman run --name myguacd --hostname myguacd --ip $GUACD_IP -d guacamole/guacd:0.9.14
 
 # postgres port is 5432
 echo "2) Creating postgres container"
-podman pull docker.io/guacamole/guacamole:1.0.0
-podman run --rm guacamole/guacamole:1.0.0 /opt/guacamole/bin/initdb.sh --postgres > /tmp/initdb.sql
+#podman pull docker.io/guacamole/guacamole:1.0.0
+podman run --rm localhost/myguacamole /opt/guacamole/bin/initdb.sh --postgres > /tmp/initdb.sql
 chmod 755 /tmp/initdb.sql
 
 podman pull docker.io/library/postgres:12.1
@@ -29,7 +30,7 @@ podman exec -it mypostgres  psql -Uguacamole_user  -a guacamole_db -c 'GRANT SEL
 
 # guacamole port is 8080
 echo "3) Creating guacamole container"
-podman run --name myguacamole --hostname=myguacamole --ip $GUACAMOLE_IP --add-host=myguacd:$GUACD_IP --add-host=mypostgres:$POSTGRES_IP --add-host=mycentosvnc:$CENTOSVNC_IP -e GUACD_HOSTNAME=myguacd -e GUACD_PORT=4822 -e POSTGRES_HOSTNAME=mypostgres -e POSTGRES_DATABASE=guacamole_db -e POSTGRES_USER=guacamole_user -e POSTGRES_PASSWORD=guacamole_pass -d guacamole/guacamole:1.0.0
+podman run --name myguacamole --hostname=myguacamole --ip $GUACAMOLE_IP --add-host=myguacd:$GUACD_IP --add-host=mypostgres:$POSTGRES_IP --add-host=mycentosvnc:$CENTOSVNC_IP -e GUACD_HOSTNAME=myguacd -e GUACD_PORT=4822 -e POSTGRES_HOSTNAME=mypostgres -e POSTGRES_DATABASE=guacamole_db -e POSTGRES_USER=guacamole_user -e POSTGRES_PASSWORD=guacamole_pass -d localhost/myguacamole
 
 # vnc port is 5901
 podman run --name mycentosvnc --hostname=mycentosvnc --ip $CENTOSVNC_IP -p ${PUB_PORT}:5901 --privileged --shm-size=1024m --memory=1024m --memory-swap=1024m -d localhost/mycentosvnc
